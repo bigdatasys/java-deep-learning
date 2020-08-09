918dacdb0fed771a1e55460760e8b8938c9fa066
bigdatasys <dr.maciej.majewski@gmail.com>
2020-08-09 14:49:04 +0200

    update

--- src/main/java/DeepLearning/DeepLearningApp.java
+++ src/main/java/DeepLearning/DeepLearningApp.java
@@ -1,7 +1,10 @@
 package DeepLearning;
 
-public class DeepLearningApp {
-    public static void main(String[] args) {
+import java.io.IOException;
 
+public class DeepLearningApp {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        SentimentClassifier classifier = new SentimentClassifier();
+        classifier.classify("sentiment.csv","sentiment-test.csv");
     }
 }
